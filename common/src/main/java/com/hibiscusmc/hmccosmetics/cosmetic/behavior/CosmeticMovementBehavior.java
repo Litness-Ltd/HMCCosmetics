8c119c78dbfe597e5014170987b9714c990a98b3
Tech <debitcardzpaypal@gmail.com>
2025-10-13 14:58:02 -0400

    fix api

--- common/src/main/java/com/hibiscusmc/hmccosmetics/cosmetic/behavior/CosmeticMovementBehavior.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/cosmetic/behavior/CosmeticMovementBehavior.java
@@ -3,6 +3,9 @@ package com.hibiscusmc.hmccosmetics.cosmetic.behavior;
 import com.hibiscusmc.hmccosmetics.user.CosmeticUser;
 import org.bukkit.Location;
 
+/**
+ * Updates cosmetics whenever a player moves.
+ */
 public interface CosmeticMovementBehavior {
     void dispatchMove(
         final CosmeticUser user,
